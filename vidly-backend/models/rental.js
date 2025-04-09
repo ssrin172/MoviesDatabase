1c8a76e396b64dec4cd6252edaa85f5f5c4545db
ssrin172 <ssrin172@asu.edu>
2025-04-08 18:04:29 -0700

    Adding a rentals model to keep track of movies rented

---
+++ vidly-backend/models/rental.js
@@ -0,0 +1,58 @@
+import Joi from "joi";
+import mongoose from "mongoose";
+export const Rental = mongoose.model(
+  "Rental",
+  new mongoose.Schema({
+    customer: {
+      type: new mongoose.Schema({
+        name: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50,
+        },
+        isGold: {
+          type: Boolean,
+          default: false,
+        },
+        phone: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50,
+        },
+      }),
+      required: true,
+    },
+    movie: {
+      type: new mongoose.Schema({
+        title: {
+          type: String,
+          required: true,
+          trim: true,
+          minlength: 1,
+          maxlength: 255,
+        },
+        dailyRentalRate: {
+          type: Number,
+          required: true,
+          min: 0,
+          max: 255,
+        },
+      }),
+      required: true,
+    },
+    dateOut: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    dateReturned: {
+      type: Date,
+    },
+    rentalFee: {
+      type: Number,
+      min: 0,
+    },
+  })
+);
