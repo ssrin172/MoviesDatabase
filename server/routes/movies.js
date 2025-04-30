95654dffc4d67b96ec6d064b63369dd7a46bb67a
ssrin172 <ssrin172@asu.edu>
2025-04-29 21:08:41 -0700

    Fixing add movie option not working

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -20,7 +20,7 @@ router.get("/:id", async (req, res) => {
 
 //add a movie
 router.post("/", async (req, res) => {
-  const genre = await Genre.findOne({ name: req.body.genre });
+  const genre = await Genre.findById(req.body.genreId);
   if (!genre) return res.status(400).send("Invalid genre.");
 
   let newMovie = new Movie({
